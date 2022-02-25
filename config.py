5d09afd3c6518ba45f47cd159d46dee40d80c67a
Fishball_Noodles <34223146+TheReaper62@users.noreply.github.com>
2022-02-25 08:43:26 +0800

    Initial Commit
    Limitation on Mobile to rename Procfile to exclude .txt

---
+++ config.py
@@ -0,0 +1,9 @@
+attire = [
+    'Full Uniform',
+    'Wear Polo Tee to school, bring PE Attire',
+    'Wear Any College based Uniform',
+    'Wear Polo Tee to school, bring PE Attire',
+    'Wear Any College based Uniform',
+    "It's a Weekend",
+    "It's a Weekend",
+]
---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+import os
+import discord
+from replit import db
+from datetime import datetime
+
+from alive import keep_alive
+import config as CONF
+
+# Fucntions
+def match(raw,words):
+    return any([i in raw.lower() for i in words])
+
+client = discord.Bot()
+
+@client.event
+async def on_ready():
+    print("Bot Online")
+
+@client.event
+async def on_message(message):
+    if match(message.content,['hi','hello']):
+        await message.channel.send(f'Hi {message.author.mention}!!!')
+    elif match(message.content,['attire']):
+        today = datetime.today().weekday()
+        tmr = today+1 if (today+1)<=6 else 0
+        embed = discord.Embed(
+            title = 'Attire',
+            description = f"**Today's Attire**:\n{CONF.attire[today]}\n\n**Tomorrow's Attire**\n:{CONF.attire[tmr]}",
+            colour = discord.Colour(0xE66E6B)
+        )
+        await message.channel.send(embed=embed)
+        
+    
+# Run Webserver
+keep_alive()
+
+client.run(os.environ['token'])
