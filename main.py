63213c43d4eab9f3e77db1b6c6777fcd64c32260
TheReaper62 <34223146+TheReaper62@users.noreply.github.com>
2022-02-26 21:20:56 +0800

    Purchase Feature

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Union
 import os
 import discord
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import config as CONF
 
@@ -10,7 +10,7 @@ import config as CONF
 def match(raw,words):
     return any([i in raw.lower() for i in words])
 
-client = discord.Bot()
+client = discord.Bot(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -20,6 +20,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content.startswith('|clear'):
+        limit = int(message.content.split(' ')[1]) if len(message.content.split(' ')) > 1 else 10
+        await message.channel.purge(limit=limit)
+        await message.channel.send(f"Cleared {limit} messages", delete_after=3)
     if match(message.content,['hi','hello']):
         await message.channel.send(f'Hi {message.author.mention}!!!')
     elif match(message.content,['attire']):
@@ -27,9 +31,75 @@ async def on_message(message):
         tmr = today+1 if (today+1)<=6 else 0
         embed = discord.Embed(
             title = 'Attire',
-            description = f"**Today's Attire**:\n{CONF.attire[today]}\n\n**Tomorrow's Attire**\n:{CONF.attire[tmr]}",
+            description = f"**Today's Attire**:\n{CONF.attire[today]}\n\n**Tomorrow's Attire**:\n{CONF.attire[tmr]}",
             colour = discord.Colour(0xE66E6B)
         )
         await message.channel.send(embed=embed)
         
+@client.event
+async def on_raw_reaction_add(payload):
+    emoji = payload.emoji.name
+    member = payload.member
+    channel_ = await member.guild.fetch_channel(payload.channel_id)
+    message = await channel_.fetch_message(payload.message_id)
+    if len(message.embeds) > 0:
+        cache = message.embeds[0]
+        if "Managed by 22S209 Utility" in cache.footer.text and len(cache.fields) == 1:
+            opt_in = cache.fields[0].value.split("\n")
+            if emoji == "✅" and str(member) not in opt_in:
+                opt_in.append(str(member))
+                if len(opt_in) > 1 and 'None' in opt_in:
+                    opt_in.remove('None')
+                
+                cache.remove_field(0)
+                cache.add_field(name="Opt In", value="\n".join(opt_in))
+                await message.edit(embed=cache)
+
+            elif emoji == "❌" and str(member) in opt_in:
+                opt_in.remove(str(member))
+                if len(opt_in) ==0 :
+                    opt_in = ['None']
+
+                cache.remove_field(0)
+                cache.add_field(name="Opt In", value="\n".join(opt_in))
+                await message.edit(embed=cache)
+
+def timestamps(key=None):
+    stamps = {
+        '10 Minutes': datetime.now()+timedelta(minutes=10),
+        '30 Minutes' : datetime.now()+timedelta(minutes=30),
+        '1 Hour' : datetime.now()+timedelta(hours=1),
+        '3 Hours' : datetime.now()+timedelta(hours=3),
+        '6 Hours' : datetime.now()+timedelta(hours=6),
+        '24 Hours' : datetime.now()+timedelta(days=1),
+        'Tomorrow Morning (8:00)': (datetime.now()+timedelta(days=1)).replace(hour=8),
+    }
+    if key is None:
+        return stamps
+    return int(stamps[key].timestamp()+28800)
+
+@client.slash_command(
+    guild_ids=[946223026238783599],
+    name='purchase',
+    description='Create Purchase Menu',
+)
+async def purchase(
+    ctx: discord.ApplicationContext,
+    item_name: discord.commands.Option(str, 'Name of Item', required=True),
+    item_desc: discord.commands.Option(str, 'Description of Item', required=True),
+    item_cost: discord.commands.Option(float, 'Cost of Item', required=True),
+    expiry: discord.commands.Option(str, 'Expiry of this Menu', choices=timestamps().keys(), required=True)
+):
+    item_cost = "{:.2f}".format(item_cost)
+    embed = discord.Embed(
+        title = f'Purchase of {item_name}',
+        description = f"**Item Description**: {item_desc}\n**Item Cost**: **${item_cost}**\n**Expiry**: <t:{timestamps(expiry)}>\n\nDo React Below so that the representative can help you purchase this material on your behalf. Collection and Payment will be settled later.",
+        colour = discord.Colour(0xE66E6B)
+    ).set_footer(text = f'Managed by 22S209 Utility | Created by {ctx.author}')
+    embed.add_field(name="Opt In", value='None')
+    message = await ctx.send(embed=embed)
+    await ctx.respond('Completed Without Errors\n*Only you can see this*',ephemeral=True)
+    await message.add_reaction('✅')
+    await message.add_reaction('❌')
+
 client.run(os.environ['token'])
